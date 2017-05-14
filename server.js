56d6719c01cc3500661795a1bbdc83812be898b4
Rolando Navarrete <rolando@splice.com>
2017-05-13 17:17:43 -0700

    commented out old routes

--- server.js
+++ server.js
@@ -10,17 +10,17 @@ var releasesFile = 'cache/releases.json';
 var app = express();
 app.use(express.bodyParser());
 
-app.get('/articles', function (req, res) {
-    feed.articles();
-    results = JSON.parse(fs.readFileSync(articlesFile));
-    res.json(results)
-})
+// app.get('/articles', function (req, res) {
+//     feed.articles();
+//     results = JSON.parse(fs.readFileSync(articlesFile));
+//     res.json(results)
+// })
 
-app.get('/releases', function (req, res) {
-    feed.releases();
-    results = JSON.parse(fs.readFileSync(releasesFile));
-    res.json(results)
-})
+// app.get('/releases', function (req, res) {
+//     feed.releases();
+//     results = JSON.parse(fs.readFileSync(releasesFile));
+//     res.json(results)
+// })
 
 app.post('/article', (req, res, next) => {
   const results = [];
@@ -40,6 +40,30 @@ app.post('/article', (req, res, next) => {
     [data.title, data.author, data.image, data.url, data.source]);
     // SQL Query > Select Data
     const query = client.query('SELECT * FROM items ORDER BY id ASC');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
+app.get('/articles', (req, res, next) => {
+  const results = [];
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM articles ORDER BY id ASC;');
     // Stream results back one row at a time
     query.on('row', (row) => {
       results.push(row);
