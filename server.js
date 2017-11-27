516a984dbd7c472474c8e3a99e58736d34816381
rNavarrete <kinglouis.belmont@gmail.com>
2017-11-26 21:47:25 -0800

    removed logging

--- server.js
+++ server.js
@@ -147,14 +147,10 @@ app.post('/search', (req, res, next) => {
           results.push(row);
         });
       // After all data is returned, close connection and return results
-        console.log("Are there any results here1: ", results)
-        console.log("Are there any results here2: ", results)
         return res.json(results);
         client.end();
       });
-      console.log("Are there any results here3: ", results)
     });
-  console.log("Are there any results here4: ", results)
   });
 
 
