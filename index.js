2469b48260c0318562e5d6126e44fb3d4c0d14fa
rNavarrete <kinglouis.belmont@gmail.com>
2018-06-17 02:45:40 -0700

    updating sort order

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ router.get('/articles', (req, res, next) => {
       return res.status(500).json({success: false, data: err});
     }
     // SQL Query > Select Data
-    const query = client.query('SELECT * FROM articles ORDER BY id ASC;');
+    const query = client.query('SELECT * FROM articles ORDER BY id DESC;');
     // Stream results back one row at a time
     query.on('row', (row) => {
       results.push(row);
