5cae18d1745866bcc7d9b61ae082788989d5044c
rNavarrete <kinglouis.belmont@gmail.com>
2018-08-02 23:05:56 -0700

    debugging the date

--- feeds.js
+++ feeds.js
@@ -194,6 +194,7 @@ function setArticlesToCorrectOrder(source1, source2) {
                     // grab the current date
                     var date = new Date();
                     date = (date.getMonth() + 1) + "." + date.getDate();
+                    console.log("This is the raw date: ", date)
                     console.log("This is the current date: ", date)
                     console.log("This is the releaseDate date: ", releaseDate)
                     if (parseFloat(releaseDate) >= parseFloat(date) ) {
